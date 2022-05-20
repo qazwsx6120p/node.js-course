b76b42427cd6402b00c2a1460e2d2146cab85ded
Seven <qazwsx6120p@gmail.com>
2022-05-20 17:42:07 +0800

    更新

--- crawler/crawler-async.js
+++ crawler/crawler-async.js
@@ -1,39 +1,31 @@
-
 const axios = require('axios');
 const fs = require('fs');
 
 let readStock = new Promise(function (resolve, reject) {
-    fs.readFile('stock.txt', 'utf-8', (err, data) => {
-        if (err) {
-            // 錯誤了
-            reject(err);
-        } else {
-            resolve(data);
-        }
-    });
+  fs.readFile('stock.txt', 'utf-8', (err, data) => {
+    if (err) {
+      // 錯誤了
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
 });
 
-// readStock.then((a)=>{
-//     console.log(a)
-// })
-
-
 async function main() {
-    let num = await readStock
-    axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {  //<-老師!不好意思，問一下為何這邊可以省略return不會報錯(crawler-promise.js檔案沒有return印出來結果會是undefined)
-        params: {
-            // 設定 query string
-            response: 'json',
-            date: '20220301',
-            stockNo: num,
-        },
-    }).then((response) => {
-        console.log(response.data);
+  let num = await readStock;
+  axios
+    .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+      //<-老師!不好意思，問一下為何這邊可以省略return不會報錯(crawler-promise.js檔案沒有return印出來結果會是undefined)
+      params: {
+        // 設定 query string
+        response: 'json',
+        date: '20220301',
+        stockNo: num,
+      },
     })
+    .then((response) => {
+      console.log(response.data);
+    });
 }
 main();
-
-
-
-
-
---
+++ crawler/crawler-async2.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const fs = require('fs/promises');
+
+
+
+async function main() {
+  //讀取stock檔案
+  let num = await fs.readFile("stock.txt", "utf-8");
+  axios
+    .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+      params: {
+        // 設定 query string
+        response: 'json',
+        date: '20220301',
+        stockNo: num,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    });
+}
+main();
