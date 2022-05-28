5da108fe9c2f20c8bd8b9cd9a7a1b380143bf717
seven <qazwsx6120p@gmail.com>
2022-05-28 22:35:08 +0800

    更新

--- simple-express/sever.js
+++ simple-express/sever.js
@@ -7,16 +7,18 @@ const app = express();
 const path = require('path');
 
 // 使用第三方開發的中間件 cors
+// 為串接react跟後端資料庫的中間套件
 const cors = require('cors');
 app.use(cors());
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 // 這裡不會像爬蟲那樣，只建立一個連線 (mysql.createConnection)
 // 但是，也不會幫每一個 request 都分別建立連線
 // ----> connection pool
-let pool = mysql
+let pool = mysql //mysql.createPool() 建立連線池，從連線池中獲取連線
   .createPool({
+    //process.env設置環境變數
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -24,8 +26,7 @@ let pool = mysql
     database: process.env.DB_NAME,
     // 為了 pool 新增的參數
     connectionLimit: 10,
-  })
-  .promise();
+  });
 
 // client - server
 // client send request -------> server
@@ -40,8 +41,6 @@ let pool = mysql
 // middleware 中有兩種結果：
 // 1. next: 往下一個中間件去
 // 2. response: 結束這次的旅程 (req-res cycle)
-
-
 
 // express 處理靜態資料
 // 靜態資料: html, css 檔案, javascript 檔案, 圖片, 影音檔...
@@ -78,6 +77,7 @@ app.use((request, response, next) => {
 // method: get, post, put, delete, ...
 // 路由中間件
 app.get('/', (request, response, next) => {
+  console.log(pool);
   console.log('首頁CCC');
   // 送回 response，結束了 request-response cycle
   response.send('首頁');
@@ -97,22 +97,30 @@ app.get('/error', (req, res, next) => {
   // --> 都會跳去錯誤處理中間件
 });
 
-
-
 // RESTful API
 // 取得 stocks 的列表
 app.get('/stocks', async (req, res, next) => {
   console.log('我是股票列表');
+  //從資料庫裡撈資料
   let [data, fields] = await pool.execute('SELECT * FROM stocks');
+  //因為是客戶端的請求，所以是res，並用json格式回傳
   res.json(data);
 });
 
 // 取得某個股票 id 的資料
+//:stockId為變數  <---記得加:
 app.get('/stocks/:stockId', async (req, res, next) => {
+
   // 取得網址上的參數 req.params
-  // req.params.stockId
+  // 利用req.params.取得網站變數 此方法取得網址上的參數
   console.log('get stocks by id', req.params);
-  let [data, fields] = await pool.execute('SELECT * FROM stocks WHERE id = ' + req.params.stockId);
+
+  //pool.execute(SQL語法)從路由池裡執行SQL語法
+  let [data, fields] = await pool.execute(
+
+    //從stocks資料庫中取得 id為網址get的股票id
+    'SELECT * FROM stocks WHERE id = ' + req.params.stockId
+  );
 
   console.log('query stock by id:', data);
   // 空資料(查不到資料)有兩種處理方式：
--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
 import { useState, useEffect } from 'react';
 
 const Stock = () => {
+  //useState 會回傳一個包含兩個值的 array，第一個值是 state、
+  //第二個值是用來更新 state 的函式
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
     let getStocks = async () => {
+      //取得 http://localhost:3001/stocks API
       let response = await axios.get('http://localhost:3001/stocks');
+      //將response set到react的狀態裡面
       setStocks(response.data);
     };
+    //呼叫函示
     getStocks();
-  }, []);
+  }, []); //<--如果為空陣列只會在第一次傳入時執行以上代碼
 
   return (
     <div>
@@ -21,7 +27,9 @@ const Stock = () => {
         return (
           <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
             <Link to={`/stock/${stock.id}`}>
-              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">
+                {stock.id}
+              </h2>
               <p className="text-gray-700">{stock.name}</p>
             </Link>
           </div>
