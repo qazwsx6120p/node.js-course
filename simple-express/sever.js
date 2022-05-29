c9cb07da9d13ac585fb046f0d1d49e3b0cb0ec14
Seven <qazwsx6120p@gmail.com>
2022-05-29 16:32:36 +0800

    更新

--- simple-express/sever.js
+++ simple-express/sever.js
@@ -7,18 +7,16 @@ const app = express();
 const path = require('path');
 
 // 使用第三方開發的中間件 cors
-// 為串接react跟後端資料庫的中間套件
 const cors = require('cors');
 app.use(cors());
 
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 // 這裡不會像爬蟲那樣，只建立一個連線 (mysql.createConnection)
 // 但是，也不會幫每一個 request 都分別建立連線
 // ----> connection pool
-let pool = mysql //mysql.createPool() 建立連線池，從連線池中獲取連線
+let pool = mysql
   .createPool({
-    //process.env設置環境變數
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -26,7 +24,9 @@ let pool = mysql //mysql.createPool() 建立連線池，從連線池中獲取連
     database: process.env.DB_NAME,
     // 為了 pool 新增的參數
     connectionLimit: 10,
-  });
+    dateStrings: true,
+  })
+  .promise();
 
 // client - server
 // client send request -------> server
@@ -41,6 +41,17 @@ let pool = mysql //mysql.createPool() 建立連線池，從連線池中獲取連
 // middleware 中有兩種結果：
 // 1. next: 往下一個中間件去
 // 2. response: 結束這次的旅程 (req-res cycle)
+
+// express SSR 的做法
+// 設定 express 視圖檔案放在哪裡
+app.set('views', path.join(__dirname, 'views'));
+// 設定 express要用哪一種樣版引擎 (template engine)
+// npm i pug
+app.set('view engine', 'pug');
+
+// extended: false --> querystring
+// extended: true --> qs
+// app.use(express.urlencoded({ extended: true }));
 
 // express 處理靜態資料
 // 靜態資料: html, css 檔案, javascript 檔案, 圖片, 影音檔...
@@ -77,7 +88,6 @@ app.use((request, response, next) => {
 // method: get, post, put, delete, ...
 // 路由中間件
 app.get('/', (request, response, next) => {
-  console.log(pool);
   console.log('首頁CCC');
   // 送回 response，結束了 request-response cycle
   response.send('首頁');
@@ -97,41 +107,72 @@ app.get('/error', (req, res, next) => {
   // --> 都會跳去錯誤處理中間件
 });
 
+app.get('/ssr', (req, res, next) => {
+  // 會去 views 檔案夾裡找 index.pug
+  // 第二個參數: 資料物件，會傳到 pug 那邊去，pug 可以直接使用
+  res.render('index', {
+    stocks: ['台積電', '長榮', '聯發科'],
+  });
+});
+
 // RESTful API
 // 取得 stocks 的列表
 app.get('/stocks', async (req, res, next) => {
   console.log('我是股票列表');
-  //從資料庫裡撈資料
   let [data, fields] = await pool.execute('SELECT * FROM stocks');
-  //因為是客戶端的請求，所以是res，並用json格式回傳
   res.json(data);
 });
 
 // 取得某個股票 id 的資料
-//:stockId為變數  <---記得加:
 app.get('/stocks/:stockId', async (req, res, next) => {
-
   // 取得網址上的參數 req.params
-  // 利用req.params.取得網站變數 此方法取得網址上的參數
+  // req.params.stockId
   console.log('get stocks by id', req.params);
 
-  //pool.execute(SQL語法)從路由池裡執行SQL語法
-  let [data, fields] = await pool.execute(
+  // RESTful 風格之下，鼓勵把這種過濾參數用 query string 來傳遞
+  // /stocks/:stockId?page=1
+  // 1. 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  // req.query = {}
+  // 如果網址上沒有 page 這個 query string，那 req.query.page 會是 undefined
+  // undefined 會是 false，所以 page 就被設定成 || 後面那個數字
+  // https://developer.mozilla.org/zh-CN/docs/Glossary/Falsy
+  let page = req.query.page || 1;
+  console.log('current page', page);
 
-    //從stocks資料庫中取得 id為網址get的股票id
-    'SELECT * FROM stocks WHERE id = ' + req.params.stockId
-  );
+  // 2. 取得目前的總筆數
+  let [allResults, fields] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ?', [req.params.stockId]);
+  const total = allResults.length;
+  console.log('total:', total);
 
-  console.log('query stock by id:', data);
-  // 空資料(查不到資料)有兩種處理方式：
-  // 1. 200OK 就回 []
-  // 2. 回覆 404
-  if (data.length === 0) {
-    // 這裡是 404 範例
-    res.status(404).json(data);
-  } else {
-    res.json(data);
-  }
+  // 3. 計算總共有幾頁
+  // Math.ceil 1.1 => 2   1.05 -> 2
+  const perPage = 5; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+  console.log('lastPage:', lastPage);
+
+  // 4. 計算 offset 是多少（計算要跳過幾筆）
+  // 在第五頁，就是要跳過 4 * perPage
+  let offset = (page - 1) * perPage;
+  console.log('offset:', offset);
+
+  // 5. 取得這一頁的資料 select * from table limit ? offet ?
+  let [pageResults] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date DESC LIMIT ? OFFSET ?', [req.params.stockId, perPage, offset]);
+
+  // test case:
+
+  // 正面: 沒有page, page=1, page=2, page=12 (因為總共12頁)
+  // 負面: page=-1, page=13, page=空白(page=1), page=a,...
+  // 6. 回覆給前端
+  res.json({
+    // 用來儲存所有跟頁碼有關的資訊
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
+    // 真正的資料
+    data: pageResults,
+  });
 });
 
 // 這個中間件在所有路由的後面
--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 import { useState, useEffect } from 'react';
 
 const Stock = () => {
